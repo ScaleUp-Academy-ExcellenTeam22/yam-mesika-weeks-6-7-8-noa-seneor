097b02521a484cb6cd450a923eab37e4ca778950
noa-seneor <100723117+noa-seneor@users.noreply.github.com>
2022-05-08 21:16:44 +0200

    Update remember.py

--- remember.py
+++ remember.py
@@ -1,24 +1,19 @@
 from PIL import Image
 
 
-def decipher(png_path):
+def decipher(png_path) -> str:
     """
     :param png_path: path of the cipher file
     :return: return the hidden message
     """
-    img = Image.open(png_path)
-    img = img.convert('RGB')
-    width, height = img.size[0], img.size[1]
-    secret_msg = ''
-    for w in range(width):
-        for h in range(height):
-            # (1,1,1) dark color  in rgb
-            if img.getpixel((w, h)) == (1, 1, 1):
-                secret_msg += chr(h)
-    return secret_msg
+    image = Image.open(png_path)
+    width, height = image.size[0], image.size[1]
+    # 1 is black color
+    secret_msg = list(map(lambda x: chr(x), [h for w in range(width) for h in range(height) if image.getpixel((w, h)) ==
+                                             1]))
+    return ''.join(secret_msg)
 
 
 if __name__ == '__main__':
     print(decipher(r'C:\Users\noase\Downloads\code.png'))
-
 
